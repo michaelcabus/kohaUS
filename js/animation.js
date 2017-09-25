223cc7d0e7f319a57f5f109059f25282c1db64f2
michaelcabus <cabuscreates@gmail.com>
2017-09-25 00:09:15 -0400

    first commit

---
+++ js/animation.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $('.frame_two').hide();
+  $('.frame_three').hide();
+  $('.frame_four').hide();
+
+$('.frame-one').hide().delay(4000).fadeIn(2200);
+$('.frame-one').hide().delay(20).fadeOut(2200);
+$('.frame_two').hide().delay(9000).fadeIn(2200);
+$('.frame_two').hide().delay(20).fadeOut(2200);
+$('.frame_three').delay(14000).fadeIn(2200);
+$('.frame_four').delay(14000).fadeIn(2200);
+
+
+});
