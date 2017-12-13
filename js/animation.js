311da7664fa78d192aedb5a5db0a15873bb83830
michaelcabus <cabuscreates@gmail.com>
2017-12-13 10:28:19 -0500

    content updates

--- js/animation.js
+++ js/animation.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
   $('.frame_four').hide();
 
 $('.frame-one').hide().delay(4000).fadeIn(2200);
-$('.frame-one').hide().delay(20).fadeOut(2200);
-$('.frame_two').hide().delay(9000).fadeIn(2200);
-$('.frame_two').hide().delay(20).fadeOut(2200);
-$('.frame_three').delay(14000).fadeIn(2200);
-$('.frame_four').delay(14000).fadeIn(2200);
+$('.frame-one').hide().delay(3000).fadeOut(1200);
+$('.frame_two').hide().delay(12000).fadeIn(2200);
+$('.frame_two').hide().delay(3000).fadeOut(1200);
+$('.frame_three').delay(20000).fadeIn(2200);
+$('.frame_four').delay(20000).fadeIn(2200);
 
 
 });
